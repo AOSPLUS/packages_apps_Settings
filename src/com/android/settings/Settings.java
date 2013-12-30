fcf051f73920fb2c319a0507f485fb02cfa2fec4
Lars Greiss <kufikugel@googlemail.com>
2013-12-30 15:27:01 +0700

    [2/3] Settings: QuietHours (SlimRoms)
    add support QuiteHours
    
    Squash commit from:
    
     Quiet Hours (2/2) thanks cm
    
    remove as well depreciated ontreeclick
    
    Change-Id: Id61ced473589e1f4a6ca30e4d3302c01f7b91ae2
    
    by Kufikugel
    
     Settings: QuietHours AutoSMS && Bypass Alarm
    
    AOKP kanged DarkSlim...
    	An original Slim feature.
    
    Hai Slim =]
    	Have an original AOKP feature.
    
    PS2: Whitespace
    PS3: Use slim_arrays
    PS4: Remove unused imports &&
    	Use StateListDrawable
    	for Alarm Dismiss (color-
    	changing button)
    PS5: moved services into slim folder to have all seperated
         minor code style corrections
         add dialog instance handling in QuietHours.java
    PS6: the right class folder is a good idea sometimes :p
    PS8: added cancel button to the dialogs
    PS9: Holo Gray
    
    Change-Id: Ia24450ac6aa60e74f8c93113a35d43b4602de189
    
    by Jubakuba
    
     Settings: Quiet Hours Logic Change (2/2)
    
    12:00am - 12:00am = 24 hours
    	(Or any "==" time)
    This makes more sense for the user
    	if they have the direct ability
    	to toggle quiet hours.
    	The feature should work as-is
    	and in a 24-hour on/off manner
    	through using the toggle.
    
    Dependant upon this patch as well:
    	https://review.slimroms.net:8443/#/c/39226/
    
    Change-Id: I20873f7c66ea20d247cb8764a475f0bb17ccfbfa
    
    by Jubakuba
    
     settings: fix QuieHours call from fb
    
    Change-Id: I3245828aa675e186344d5370b9ea71ee6520349e
    
    by Kufikugel
    
     settings: refactor QuietHours code
    
    PatchSet: use correct context
    
    Change-Id: I9391e274250128af5f0e9dea4bdfde2485d9a746
    
    by Kufikugel
    
     Quiet Hours Ringer (3/3)
    
    Change-Id: Ia4b167654867425b8d7cac952de094b16002da02
    
    by Jubakuba
    
    Change-Id: I9ca1894f3d2a7a949082e97ba7e959d018495896
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/custom_arrays.xml
    	res/values/custom_strings.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SoundSettings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -94,6 +94,7 @@ import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.WifiEnabler;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
+import com.android.settings.slim.QuietHours;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -355,7 +356,8 @@ public class Settings extends PreferenceActivity
         PrintJobSettingsFragment.class.getName(),
         TrustedCredentialsSettings.class.getName(),
         PaymentSettings.class.getName(),
-        KeyboardLayoutPickerFragment.class.getName()
+        KeyboardLayoutPickerFragment.class.getName(),
+        QuietHours.class.getName()
     };
 
     @Override
@@ -1101,6 +1103,7 @@ public class Settings extends PreferenceActivity
     public static class LocalePickerActivity extends Settings { /* empty */ }
     public static class UserDictionarySettingsActivity extends Settings { /* empty */ }
     public static class SoundSettingsActivity extends Settings { /* empty */ }
+    public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
     public static class DisplaySettingsActivity extends Settings { /* empty */ }
     public static class DeviceInfoSettingsActivity extends Settings { /* empty */ }
     public static class ApplicationSettingsActivity extends Settings { /* empty */ }
